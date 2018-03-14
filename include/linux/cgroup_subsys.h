c2f935529b1d2d0b3b64da37705f3f735771ac85
YudiWidiyanto <yudiwidiyanto7@gmail.com>
2018-03-14 06:58:26 +0000

    Revert "block: cgroups, kconfig, build bits for BFQ-v7r8-3.18.0"
    This reverts commit c52b3e8deb0b6281bab389d7bb4f6e7cde83ee2f.

--- include/linux/cgroup_subsys.h
+++ include/linux/cgroup_subsys.h
@@ -39,14 +39,6 @@ SUBSYS(freezer)
 SUBSYS(net_cls)
 #endif
 
-#if IS_ENABLED(CONFIG_BLK_CGROUP)
-SUBSYS(blkio)
-#endif
-
-#if IS_ENABLED(CONFIG_CGROUP_BFQIO)
-SUBSYS(bfqio)
-#endif
-
 #if IS_ENABLED(CONFIG_CGROUP_PERF)
 SUBSYS(perf_event)
 #endif
